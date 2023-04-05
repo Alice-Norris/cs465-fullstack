84f115e63f51470b995bd381d4ba0c4f9e81f1ae
Alice Kneller <alice.norris@snhu.edu>
2023-04-05 06:08:11 -0400

    Meals now also load from database instead of JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -4,13 +4,48 @@ const request = require('request')
 const apiOptions = {
   server: 'http://localhost:3000'
 }
-// parsing meal data to be inserted into handlebars partial
-const foods = JSON.parse(fs.readFileSync('./data/meals.json'))
-/* GET meals view */
-const meals = (req, res) => {
-  res.render('meals', {title: 'Meals' , foods});
+
+const renderMealList = (req, res, responseBody) => {
+  let message = null;
+  let pageTitle = process.env.npm_package_description + ' - Meals';
+  if(!(responseBody instanceof Array)) {
+    message = 'API lookup error';
+    responseBody = [];
+  } else {
+    if(!responseBody.length) {
+      message = 'No meals exist in database!';
+    }
+  }
+  console.log(JSON.stringify(responseBody));
+  res.render('meals', {
+    title: pageTitle,
+    meals: responseBody,
+    message
+  });
 };
 
+const mealList = (req, res) => {
+  const path = '/api/meals';
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+  };
+
+  console.info('>> mealsController.mealList calling ' + requestOptions.url);
+
+  request(
+    requestOptions,
+    (err, { statusCode }, body) => {
+      if (err) {
+        console.error(err);
+      }
+      renderMealList(req, res, body);
+    }
+  )
+}
+
 module.exports = {
-  meals
+  mealList,
+  renderMealList
 };
